594f6fa0dd85d52239eceab0e63fb07a7acc08f9
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-05-29 01:41:37 +0600

    TalentMesh ScrollToTop-functionality done.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/Shared/Navbar/Navbar';
 import Footer from '../components/Shared/Footer/Footer';
+import ScrollToTop from '../components/Shared/ScrollToTop/ScrollToTop';
 
 const MainLayout = () => {
     return (
         <div className='bg-gradient-to-r from-blue-50 via-blue-100 to-blue-200'>
+            <ScrollToTop></ScrollToTop>
+
             <div>
                 <Navbar></Navbar>
             </div>
